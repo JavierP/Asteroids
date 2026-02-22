7f0740227c65cdcfedada89ddb7eb4829351e3ff
JavierP <silver9ga@gmail.com>
2026-02-22 00:36:19 -0600

    Added player rotation and update

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
+from circleshape import CircleShape
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2   
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+    keepGameRunning = True
+    player = Player(x,y)
+
+
+    while keepGameRunning:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-            screen.fill("black")
-            pygame.display.flip()
-
+                keepGameRunning = False
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
