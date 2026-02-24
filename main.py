87da75fc2588f44df449a803de47a8b669f44c41
JavierP <silver9ga@gmail.com>
2026-02-23 23:59:22 -0600

    added groups to make updating and drawing more manageable and more scalable

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
-from logger import log_state
 from player import Player
 from circleshape import CircleShape
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2   
+    x, y = SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     keepGameRunning = True
-    player = Player(x,y)
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
 
     while keepGameRunning:
         log_state()
+        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 keepGameRunning = False
-        player.update(dt)
-        screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
